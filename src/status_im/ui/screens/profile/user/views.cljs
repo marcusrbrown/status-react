aee05c237f032edb8eae04aecc84a503f4555526
Dmitry Novotochinov <dmitry.novot@gmail.com>
2019-09-09 13:36:25 +0300

    add keycard settings

--- src/status_im/ui/screens/profile/user/views.cljs
+++ src/status_im/ui/screens/profile/user/views.cljs
@@ -23,7 +23,8 @@
             [status-im.ui.screens.profile.user.styles :as styles]
             [status-im.utils.identicon :as identicon]
             [status-im.utils.platform :as platform]
-            [status-im.utils.universal-links.core :as universal-links]))
+            [status-im.utils.universal-links.core :as universal-links]
+            [status-im.utils.config :as config]))
 
 (views/defview share-chat-key []
   (views/letsubs [{:keys [address]}              [:popover/popover]
@@ -159,6 +160,13 @@
     :accessibility-label :sync-settings-button
     :accessories         [:chevron]
     :on-press            #(re-frame/dispatch [:navigate-to :sync-settings])}
+   (when (and platform/android?
+              config/hardwallet-enabled?)
+     {:icon                :main-icons/keycard
+      :title               :t/keycard
+      :accessibility-label :keycard-button
+      :accessories         [:chevron]
+      :on-press            #(re-frame/dispatch [:navigate-to :keycard-settings])})
    {:icon                :main-icons/settings-advanced
     :title               :t/advanced
     :accessibility-label :advanced-button
